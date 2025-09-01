dd8bf6f37f02a6c145f2fd23065566ec74458db9
Nitin Malviya <nitinmalviya172@gmail.com>
2025-09-01 23:03:49 +0530

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+// version 1  1.1 1.01 1.3 2 2.4
+// version 1 ---> 1.1  1.3 fixing  existing feature 
+
+//  new feature  2 3 4 5
+
+
+//  gpt 1 gpt 2 2.5  3 4 4 omini 5
+
+// android 16
+
+
+//  https get , post , put , patch, delete 
--- snap-card/js/main.js
+++ snap-card/js/main.js
@@ -1,5 +1,51 @@
-// Authentication login - sign-up - forgot password
+//  api integration 
 
-// user can add product in cart and delete
+// register
 
-// order place 
+let baseUrl = "https://api.freeapi.app/api/v1"
+
+let registerForm = document.getElementById("register-form");
+registerForm.addEventListener('submit', (event) => {
+    try {
+        event.preventDefault();
+        let successMessage = document.getElementById("success-message");
+        let username = event.target.username.value;
+        let email = event.target.email.value;
+        let password = event.target.password.value
+
+        register({ username, email, password, role: "USER" }).then((data) => {
+            if (data.success == true) {
+                successMessage.innerText = data.message;
+                setTimeout(() => {
+                    window.location.href = `../snap-card/login.html`
+                }, 2000)
+
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+    } catch (error) {
+        console.log(error.message);
+
+    }
+})
+
+async function register(userData) {
+    try {
+        let url = `${baseUrl}/users/register`
+        let response = await fetch(url, { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(userData) })
+        let data = await response.json();
+        console.log(data);
+
+        if (data.statusCode == 200) {
+            return data;
+        }
+        else {
+            return { success: false, message: "Registration failed...." }
+        }
+    } catch (error) {
+        console.log(error.message);
+
+    }
+
+}
